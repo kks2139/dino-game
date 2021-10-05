80167a62efcbbd91c01a8864b13b6734a04f5585
kwangsun <eyeyel@naver.com>
2021-10-05 23:26:50 +0900

    add canvas

--- src/components/GameBox.js
+++ src/components/GameBox.js
@@ -1,5 +1,5 @@
 class GameBox {
-    constructor(targ){
+    constructor({targ}){
         this.ROOT = document.createElement('div');
         this.ROOT.classList.add('.GameBox');
         targ.appendChild(this.ROOT);
@@ -7,8 +7,18 @@ class GameBox {
         this.render();
     }
 
-    render = ()=>{
+    drawGame = ()=>{
+        const canvas = this.ROOT.querySelector('#canvas');
+        const ctx = canvas.getContext('2d');
+        ctx.fillStyle = 'white';
+        ctx.fillRect(10, 100, 100, 50);
+    }
 
+    render = ()=>{
+        this.ROOT.innerHTML = `
+            <canvas id='canvas' width='500' height='300'></canvas>
+        `;
+        this.drawGame();
     }
 }
 
